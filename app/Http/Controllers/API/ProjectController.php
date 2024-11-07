34fbd93dc4d404ce0d9b6577667e33381c7af92c
Claudio-Pri <priv.claudio@gmail.com>
2024-11-07 19:09:22 +0100

    Gestiti project in api/ProjectController + paginazione

--- app/Http/Controllers/API/ProjectController.php
+++ app/Http/Controllers/API/ProjectController.php
@@ -5,11 +5,22 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+// Models
+use App\Models\Project;
+
 class ProjectController extends Controller
 {
     public function index() {
+        // $projects = Project::get();
+        // non oso seeddare più di tanto per non fare esplodere tutto 
+        $projects = Project::paginate(2);
         return response()->json([
-            'prova' => 'projects'
+            'success' => true,
+            'code' => 200,
+            'message' => 'OK',
+            'data' => [
+                'projects' => $projects
+            ]
         ]);
     }
 }
